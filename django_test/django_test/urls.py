14ffd12d6b709bd10e80fbaa6ce06f5b7c890885
Dan Herlihy <djherls@gmail.com>
2019-11-04 17:35:56 -0500

    Add sample home view

--- django_test/django_test/urls.py
+++ django_test/django_test/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from sample import views
+
 urlpatterns = [
     path('saml/', include('django_saml.urls')),
     path('admin/', admin.site.urls),
+    path('', views.home),
 ]
