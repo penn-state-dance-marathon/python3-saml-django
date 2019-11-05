dde126dbabe17d42ab35275c81f87d49b54dd293
Dan Herlihy <djherls@gmail.com>
2019-11-05 13:16:18 -0500

    Add logged_out view

--- django_test/django_test/urls.py
+++ django_test/django_test/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('saml/', include('django_saml.urls')),
     path('admin/', admin.site.urls),
     path('', views.home),
+    path('logged-out/', views.logged_out),
 ]
--- django_test/sample/views.py
+++ django_test/sample/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 @login_required
 def home(request):
     return render(request, 'sample/home.html')
+
+
+def logged_out(request):
+    return render(request, 'sample/logged_out.html')
