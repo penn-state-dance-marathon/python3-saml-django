cb4d2b4cd46a93fc3bbafbbdbc32c06783e3c70c
Dan Herlihy <djherls@gmail.com>
2019-11-05 11:08:37 -0500

    Add names to URLs

--- django_saml/urls.py
+++ django_saml/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from django_saml import views
 
 urlpatterns = [
-    path('metadata', views.metadata),
-    path('acs', views.saml_acs),
-    path('login', views.login),
-    path('logout', views.logout),
-    path('sls', views.saml_sls),
+    path('metadata', views.metadata, name='metadata'),
+    path('acs', views.saml_acs, name='acs'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
+    path('sls', views.saml_sls, name='sls'),
 ]
