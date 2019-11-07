d792d970c2cf6ee5278666dcef873e3f25e09786
Dan Herlihy <djherls@gmail.com>
2019-11-07 01:05:18 -0500

    Change python required to >=3

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3',
     extras_require={
         'test': (
             'django>=1.11',
