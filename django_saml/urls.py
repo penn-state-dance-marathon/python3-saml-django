a6e06a09c82fa530298fc536d6cea9d02e1a6632
Dan Herlihy <djherls@gmail.com>
2019-11-14 12:52:08 -0500

    1.0.1 make slash at the end of URL optional

--- django_saml/urls.py
+++ django_saml/urls.py
@@ -5,9 +5,9 @@ from django_saml import views
 app_name = 'django_saml'
 
 urlpatterns = [
-    url(r'metadata$', views.metadata, name='metadata'),
-    url(r'acs$', views.saml_acs, name='acs'),
-    url(r'login$', views.login, name='login'),
-    url(r'logout$', views.logout, name='logout'),
-    url(r'sls$', views.saml_sls, name='sls'),
+    url(r'metadata/?$', views.metadata, name='metadata'),
+    url(r'acs/?$', views.saml_acs, name='acs'),
+    url(r'login/?$', views.login, name='login'),
+    url(r'logout/?$', views.logout, name='logout'),
+    url(r'sls/?$', views.saml_sls, name='sls'),
 ]
