855bc27da0cc94ec2d13008a672e0fbf51164629
Dan <djherls@gmail.com>
2019-12-03 01:09:19 -0500

    Update setup.py version to 1.1.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="python3-saml-django",
-    version="1.0.2",
+    version="1.1.0",
     author="THON Technology",
     author_email="technology@thon.org",
     description="Implement SAML Single Sign-On in your Django project quickly and easily.",
